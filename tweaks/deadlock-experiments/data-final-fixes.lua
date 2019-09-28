de9f41db1d33c8208374e1258deb7f7c7a5b932d
billbo99 <cactus_computers-github@yahoo.com>
2019-09-28 13:30:54 +0100

    Gold and Lead direct stack smelting

--- tweaks/deadlock-experiments/data-final-fixes.lua
+++ tweaks/deadlock-experiments/data-final-fixes.lua
@@ -2,25 +2,24 @@ if not settings.startup["deadlock-experiments"].value then
     return
 end
 
--- need to do things with sand for Darkstar
-local function add_sand_to_deadlock()
-    deadlock.add_stack("sand", nil, "deadlock-stacking-1", 32, "item")
-    local sand_density = deadlock.get_item_stack_density("sand", "item")
-    local sand = {
+-- need to do things with Darkstar ores to get them to work with deadlock
+local function add_ore_to_deadlock(ingredient, ingredient_quantity , result, recipe_energy_required)
+    local ingredient_density = deadlock.get_item_stack_density(ingredient, "item")
+    local recipe = {
         type = "recipe",
-        name = "stacked-glass",
+        name = "stacked-" .. result,
         category = "smelting",
-        energy_required = 18.5 * sand_density,
-        ingredients = {{ "deadlock-stack-sand", 5}},
+        energy_required = recipe_energy_required * ingredient_density,
+        ingredients = {{ "deadlock-stack-" .. ingredient, ingredient_quantity}},
     }
-    data:extend({sand})
     if settings.startup["experiments-directsmelting-outputstacks"].value then
-        sand.result = "deadlock-stack-glass"
+        recipe.result = "deadlock-stack-" .. result
     else
-        sand.result = "glass"
-        sand.result_count = sand_density
+        recipe.result = result
+        recipe.result_count = ingredient_density
     end
-
+    log(serpent.block(recipe))
+    data:extend({recipe})
 end
 
 -- helper function to test an array for a value
@@ -52,6 +51,7 @@ local function rerun_densityoverride()
             for _, item_type in ipairs(item_types) do
                 if data.raw[item_type][parent_item] then
                     data.raw.item[k].stack_size = math.floor((data.raw[item_type][parent_item].stack_size / deadlock_stack_size) * multiplier)
+                    log(item_type .. " .. " .. parent_item .. " .. " .. tostring(data.raw.item[k].stack_size))
                 end
             end
         end
@@ -60,14 +60,20 @@ end
 
 -- deadlock-experiments needs to have its dependencies changed to that it comes after all the other deadlock stacking mods that are out there.
 if settings.startup["darkstar"].value then
-    add_sand_to_deadlock()
+
+    deadlock.add_stack("sand", nil, "deadlock-stacking-1", 32, "item")
+
+    add_ore_to_deadlock("sand", 5, "glass", 18.5)
+    add_ore_to_deadlock("gold-ore", 2, "gold-plate", 7.5)
+    add_ore_to_deadlock("lead-ore", 2, "lead-plate", 9.5)
+
     rerun_densityoverride()
 
     -- Darkstar has 25 levels of modules,  need to update the limitation on them all to allow the direct smelting of stacks to get the bonus too.
-    for _, recipe in ipairs({"stacked-copper-plate", "stacked-iron-plate", "stacked-stone-brick", "stacked-steel-plate", "deadlock-stacks-stack-gold-plate", "deadlock-stacks-stack-lead-plate", "stacked-glass"}) do
+    for _, recipe in ipairs({"stacked-copper-plate", "stacked-iron-plate", "stacked-stone-brick", "stacked-steel-plate", "stacked-gold-plate", "stacked-lead-plate", "stacked-glass"}) do
         for i = 1, 25 do
             local module_name = string.format("productivity-module-%d", i)
-            if data.raw.module[module_name] and data.raw.module[module_name].limitation then
+            if data.raw.recipe[recipe] and data.raw.module[module_name] and data.raw.module[module_name].limitation then
                 if not has_value(data.raw.module[module_name].limitation, recipe) then
                     table.insert(data.raw.module[module_name].limitation, recipe)
                 end
